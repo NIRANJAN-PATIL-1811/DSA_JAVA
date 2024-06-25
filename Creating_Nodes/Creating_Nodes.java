f56514eb5e64e9f128aa1778198cce4d8c92f41f
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2024-06-25 12:47:02 +0530

    Add new program

--- Creating_Nodes/Creating_Nodes.java
+++ Creating_Nodes/Creating_Nodes.java
@@ -2,6 +2,6 @@ package DSA_JAVA.Creating_Nodes;
 
 public class Creating_Nodes {
     public static void main(String args[]){
-        System.out.println("hello Neer");
+        System.out.println("hello Dear");
     }
 }
